c70b4fd0cbb69180b202e679d690e651db6b7f3a
choiwj <choiwj0706@naver.com>
2020-06-03 20:32:01 +0900

    map change test2

--- BreakoutGame/src/MapBuilder.java
+++ BreakoutGame/src/MapBuilder.java
@@ -20,7 +20,7 @@ public class MapBuilder {
 	}
 	public void stage2() {
 		for (int y = 0; y < map.map.length; y++) {
-			for (int x = 0; x < 10 ; x++) {
+			for (int x = 0; x < 15 ; x++) {
 				if (c.continePunct(x, x)) {
 					map.setBrickValue(1, x, y);
 				}
