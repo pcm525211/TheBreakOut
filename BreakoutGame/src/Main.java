524b6bc75df67c622794dc8c5ec52dcf742af557
choiwj <choiwj0706@naver.com>
2020-06-03 14:04:51 +0900

    .

--- BreakoutGame/src/Main.java
+++ BreakoutGame/src/Main.java
@@ -1,14 +1,18 @@
+import java.awt.Container;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 		JFrame obj = new JFrame();
+		obj.setPreferredSize(new Dimension(800,600));
 		obj.setLayout(null);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		Gameplay gamePlay = new Gameplay();
-		obj.setContentPane(gamePlay);
+		SelectStage selectstage = new SelectStage();
+		obj.setContentPane(selectstage);
 		obj.setTitle("The Breakout");
 		obj.setResizable(true);
 		obj.pack();
