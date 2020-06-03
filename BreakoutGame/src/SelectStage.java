a1ce59bb18265c20a7941235e0c1db96d9a7878c
choiwj <choiwj0706@naver.com>
2020-06-03 14:05:55 +0900

    .

--- BreakoutGame/src/SelectStage.java
+++ BreakoutGame/src/SelectStage.java
@@ -1,4 +1,17 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class SelectStage {
+import javax.swing.JPanel;
 
+public class SelectStage extends JPanel{
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 700;
+	
+	public void paint(Graphics g) {
+		g.setColor(Color.GRAY);
+		g.fillRect(0, 0, 800, 700);
+		g.setColor(Color.WHITE);
+		g.drawString("stage1", 400, 200);
+		g.drawString("stage2", 400, 400);
+	}
 }
