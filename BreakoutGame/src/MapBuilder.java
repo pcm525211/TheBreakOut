99d045b59ac4d90cb7860a22265bb7afd424cb56
choiwj <choiwj0706@naver.com>
2020-06-03 14:04:39 +0900

    stage2 +

--- BreakoutGame/src/MapBuilder.java
+++ BreakoutGame/src/MapBuilder.java
@@ -10,13 +10,22 @@ public class MapBuilder {
 	}
 	public void stage1() {
 		for (int y = 0; y < map.map.length; y++) {
-			for (int x = 0; x < 10 ; x++) {
+			for (int x = 0; x < map.map[y].length ; x++) {
 				if (c.continePunct(x, x)) {
 					map.setBrickValue(1, x, y);
 				}
 			}
 		}
 
+	}
+	public void stage2() {
+		for (int y = 0; y < map.map.length; y++) {
+			for (int x = 0; x < map.map[y].length ; x++) {
+				if (c.continePunct(x, x)) {
+					map.setBrickValue(1, x, y);
+				}
+			}
+		}
 	}
 
 }
